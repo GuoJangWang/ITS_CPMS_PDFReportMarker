74448ba28826b53d17eef68a471c04c013f9b28e
國嶂 王 <John@itsower.com.tw>
2023-01-09 11:09:52 +0800

    加入授權範例程式碼檔

---
+++ iText授權程式碼範例.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Licensing.Base;
+
+namespace iText.Highlevel.Chapter01 {
+    /// <author>Bruno Lowagie (iText Software)</author>
+    public class C01E02_Text_Paragraph_Cardo {
+        public static String KEY = "../../../resources/license/itextkey-typography.xml";
+
+        public const String DEST = "../../../results/chapter01/text_paragraph_cardo.pdf";
+
+        public const String REGULAR = "../../../resources/fonts/Cardo-Regular.ttf";
+
+        public const String BOLD = "../../../resources/fonts/Cardo-Bold.ttf";
+
+        public const String ITALIC = "../../../resources/fonts/Cardo-Italic.ttf";
+
+        public static void Main(String[] args) {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+            new C01E02_Text_Paragraph_Cardo().CreatePdf(DEST);
+        }
+
+        public virtual void CreatePdf(String dest) {
+            LicenseKey.LoadLicenseFile(new FileStream(KEY, FileMode.Open, FileAccess.Read));
+            // Initialize PDF document
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+            // Initialize document
+            Document document = new Document(pdf);
+            // Add content
+            PdfFont font = PdfFontFactory.CreateFont(REGULAR, PdfFontFactory.EmbeddingStrategy.PREFER_EMBEDDED);
+            PdfFont bold = PdfFontFactory.CreateFont(BOLD, PdfFontFactory.EmbeddingStrategy.PREFER_EMBEDDED);
+            PdfFont italic = PdfFontFactory.CreateFont(ITALIC, PdfFontFactory.EmbeddingStrategy.PREFER_EMBEDDED);
+            Text title = new Text("The Strange Case of Dr. Jekyll and Mr. Hyde").SetFont(bold);
+            Text author = new Text("Robert Louis Stevenson").SetFont(font);
+            Paragraph p = new Paragraph().SetFont(italic).Add(title).Add(" by ").Add(author);
+            document.Add(p);
+            //Close document
+            document.Close();
+        }
+    }
+}
+/*https://kb.itextpdf.com/home/it7kb/examples/itext-7-building-blocks-chapter-1-pdffont-examples#iText7BuildingBlocksChapter1:PdfFontexamples-c01e01_text_paragraph
+c01e02_text_paragraph_cardo
+C#
+*/
